c87259aa9ebf4d8421f267b6ca2080cbfb88e764
wuchuixu <364191418@qq.com>
2025-04-26 10:22:25 +0800

    fix: 增加hik支持

--- android/src/main/java/com/uupy/flutter_pda_scanner/PdaScannerPlugin.java
+++ android/src/main/java/com/uupy/flutter_pda_scanner/PdaScannerPlugin.java
@@ -19,7 +19,7 @@ public class PdaScannerPlugin implements EventChannel.StreamHandler {
     private static final String PL_SCAN_ACTION = "scan.rcv.message";
     private static final String BARCODE_DATA_ACTION = "com.ehsy.warehouse.action.BARCODE_DATA";
     private static final String HONEYWELL_SCAN_ACTION = "com.honeywell.decode.intent.action.EDIT_DATA";
-    // private static final String HIK_SCAN_ACTION = "android.intent.ACTION_SCAN_OUTPUT";
+    private static final String HIK_SCAN_ACTION = "android.intent.ACTION_SCAN_OUTPUT";
 
     private static EventChannel.EventSink eventSink;
 
@@ -29,23 +29,28 @@ public class PdaScannerPlugin implements EventChannel.StreamHandler {
             String actionName = intent.getAction();
             if (XM_SCAN_ACTION.equals(actionName) || SHINIOW_SCAN_ACTION.equals(actionName)) {
                 eventSink.success(intent.getStringExtra("scannerdata"));
-            } else if (IDATA_SCAN_ACTION.equals(actionName)) {
+            }
+            else if (IDATA_SCAN_ACTION.equals(actionName)) {
                 eventSink.success(intent.getStringExtra("value"));
-            } else if (YBX_SCAN_ACTION.equals(actionName)) {
+            }
+            else if (YBX_SCAN_ACTION.equals(actionName)) {
                 eventSink.success(intent.getStringExtra("barcode_string"));
-            } else if (PL_SCAN_ACTION.equals(actionName)) {
+            }
+            else if (PL_SCAN_ACTION.equals(actionName)) {
                 byte[] barcode = intent.getByteArrayExtra("barocode");
                 int barcodelen = intent.getIntExtra("length", 0);
                 String result = new String(barcode, 0, barcodelen);
                 eventSink.success(result);
-            } else if (HONEYWELL_SCAN_ACTION.equals(actionName) || BARCODE_DATA_ACTION.equals(actionName)) {
+            }
+            else if (HONEYWELL_SCAN_ACTION.equals(actionName) || BARCODE_DATA_ACTION.equals(actionName)) {
                 eventSink.success(intent.getStringExtra("data"));
-            } else {
+            }
+            else if (HIK_SCAN_ACTION.equals(actionName)) {
+                eventSink.success(intent.getStringExtra("barcode"));
+            }
+            else {
                 Log.i("PdaScannerPlugin", "NoSuchAction");
             }
-            // else if (HIK_SCAN_ACTION.equals(actionName)) {
-            //     eventSink.success(intent.getStringExtra("barcode"));
-            // } 
         }
     };
 
@@ -85,10 +90,10 @@ public class PdaScannerPlugin implements EventChannel.StreamHandler {
         honeywellIntentFilter.setPriority(Integer.MAX_VALUE);
         activity.registerReceiver(scanReceiver, honeywellIntentFilter);
 
-        // IntentFilter hikIntentFilter = new IntentFilter();
-        // hikIntentFilter.addAction(HIK_SCAN_ACTION);
-        // hikIntentFilter.setPriority(Integer.MAX_VALUE);
-        // activity.registerReceiver(scanReceiver, hikIntentFilter);
+        IntentFilter hikIntentFilter = new IntentFilter();
+        hikIntentFilter.addAction(HIK_SCAN_ACTION);
+        hikIntentFilter.setPriority(Integer.MAX_VALUE);
+        activity.registerReceiver(scanReceiver, hikIntentFilter);
     }
 
     public static void registerWith(PluginRegistry.Registrar registrar) {
